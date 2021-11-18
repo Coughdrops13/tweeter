393bca796fe7a0d65cab7a2a93d95ac1627abcae
Connor Roberts <connoobets@gmail.com>
2021-11-18 14:05:50 -0700

    adds escape to compensate for cross-site scripting, css tidying to make content fit in divs and more tidying

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,38 +1,21 @@
+// This is 140 characters long dsfgsdfg sdfg sgs cr gercgwercg wergc werg cwer gcwergcwer gcwer gcw ercgw erwfc rgwerg wergcwe rgcwer ce ecgr e
 
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giantsIf I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// makes sure that none of the js runs before the dom is rendered
+$(document).ready(function () {  
+  $('#error').hide(); // initially hide error since nothing is wrong yet
 
-$(document).ready(function () {  // makes sure that none of the js runs before the dom is rendered
-  const createTweetElement = function (tweet) {  // function used to create article element that contains new tweets
-    const newTweetArticle = $(
+  // function used to create article element that contains new tweets
+  const createTweetElement = function (tweet) {  
+
+    // function to avoid cross-site scripting
+    const escape = function(str) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    };
+    const safeContent = escape(tweet.content.text) // This is now a safe string to put in our new tweets
+    
+    const newTweetArticle = $( // This is the html for a new tweet
       `<article class="tweet">
         <header>
           <div>
@@ -43,7 +26,9 @@ $(document).ready(function () {  // makes sure that none of the js runs before t
             <p>${tweet.user.handle}</p>
           </div>
         </header>
-        <p class="tweet-content">${tweet.content.text}</p>
+          <div class="tweet-content">
+            <p>${safeContent}</p>
+          </div>
         <footer>
           <p class="tweet-time">${timeago.format(tweet.created_at)}</p>
           <div class="tweet-icons">
@@ -57,30 +42,68 @@ $(document).ready(function () {  // makes sure that none of the js runs before t
     return newTweetArticle;
   }
 
-  const renderTweets = function (data) {
+  const renderTweets = function (data) {// Pulls all tweets from a tweet array
+                                        // and put them in the index.html so that they 
+                                        // are rendered
     $(data).each(function () {
       const $tweet = createTweetElement(this);
-      $('.posted-tweets').append($tweet);
+      $('.posted-tweets').prepend($tweet);
     })
-    // for (const tweet of data) {
-    //   const $tweet = createTweetElement(tweet);
-    //   $('.posted-tweets').append($tweet);
-    // }
   }
 
-  renderTweets(data);
+  const loadTweets = function() { // calls render tweets after getting tweet data from /tweets
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+    })
+    .then(function(tweets) {
+      renderTweets(tweets);
+    })
+  };
+  
+  $(function () { // on form submission, if valid tweet, this funciton calls loadTweets() 
+                  // which calls renderTweets() which calls createTweetElement()
 
-  $(function () {
     const form = $('#tweet-form');
     form.submit(function (event) {
       event.preventDefault();
       const serialized = $('#tweet-form').serialize();
+      const textLengthTester = $('#tweet-form textarea').val(); // serializing turns spaces into %20 which is 3 characters
+                                                                // representing 1 space (other characters are also changed),
+                                                                // so to check a tweet's length the unserialized text must be 
+                                                                // evaluated
+      if(textLengthTester.length > 140) { // handles cases of too many characters input
+        $('#error p').text('Your tweet is too long');
+        $('#error').show().slideDown();
+        return;
+      }
+      if(textLengthTester.length === 0) { // handles case of no characters input
+        $('#error p').text(`!You must tweet at least one character!`);
+        $('#error').slideDown().show(); 
+        return;
+      }
+      // if neither of the previous cases, ajax posts new tweet to 
+      // /tweets then clears form text and calls load tweets
       $.ajax({
         method: 'POST',
         url: '/tweets',
         data: serialized,
       })
+      .then(res => {
+        // rehide error incase it was triggered earlier
+        $('#error').slideUp();
+        //clear text field
+        $('#tweet-text').val("");
+        loadTweets();
+      })
+      .catch((err)  => { // just in case something unpredictable happens
+        return alert(err, 'something went wrong');
+      })
     })
   })
+  
 
+  loadTweets(); // loads tweets that already exist at /tweets before submitting 
+                // the form prepends more
+  
 });
